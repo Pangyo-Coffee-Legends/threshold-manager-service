4c95d26dc70b53868ffb4f1407255b2abbda1f2f
oculusK <kay5990@naver.com>
2025-04-16 16:21:44 +0900

    init

---
+++ src/main/java/com/nhnacademy/thresholdmanagerservice/ThresholdManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.thresholdmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThresholdManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThresholdManagerServiceApplication.class, args);
+    }
+
+}
