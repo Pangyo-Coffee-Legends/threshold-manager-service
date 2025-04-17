bfdf2cd374130770d71f786c2bdb06d3c509f97a
oculusK <kay5990@naver.com>
2025-04-17 10:07:19 +0900

    docs : application-dev.properties 변경

---
+++ src/test/java/com/nhnacademy/thresholdmanagerservice/ThresholdManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.thresholdmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThresholdManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
