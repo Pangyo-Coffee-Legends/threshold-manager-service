bf80a9b23ea441e00abc1ce4be5e3ccd8832cf58
oculusK <kay5990@naver.com>
2025-04-17 17:29:52 +0900

    feat : ThresholdHistory domain, repository 생성 Operator.java 도 생성했음

--- src/main/java/com/nhnacademy/thresholdmanagerservice/threshold/domain/Threshold.java
+++ src/main/java/com/nhnacademy/thresholdmanagerservice/threshold/domain/Threshold.java
@@ -1,4 +1,15 @@
 package com.nhnacademy.thresholdmanagerservice.threshold.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+// TODO entity, class 내부 내용 없애야함
+@Entity
 public class Threshold {
+    @Id
+    @GeneratedValue
+    @Column(name = "threshold_id")
+    private Long thresholdId;
 }
